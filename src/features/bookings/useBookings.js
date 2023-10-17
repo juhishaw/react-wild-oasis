7b37beee8c0106b0428b6e0798c6320173bbf966
Juhi Shaw <juhishaw@Juhis-MacBook-Air.local>
2023-10-17 13:39:06 +0530

    API side filtering-bookigs

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,24 @@
+import { useSearchParams } from "react-router-dom";
 import { getBookings } from "../../services/apiBookings";
 import { useQuery } from "@tanstack/react-query";
 
 export const useBookings = () => {
+  const [searchParams] = useSearchParams();
+
+  //Filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, bookings, error };
