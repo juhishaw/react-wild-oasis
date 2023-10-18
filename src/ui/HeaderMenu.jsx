b06fb4b2edca5e72b88e13d8504bc999c97a2550
Juhi Shaw <juhishaw@Juhis-MacBook-Air.local>
2023-10-18 12:18:39 +0530

    bug by varcel deploy fixed

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -3,7 +3,7 @@ import Logout from "../features/authentication/Logout";
 import ButtonIcon from "./ButtonIcon";
 import { HiOutlineUser } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
-import DarkModeToggle from "./darkModeToggle";
+import DarkModeToggle from "./DarkModeToggle";
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
